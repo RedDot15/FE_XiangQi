5662aae4b6b83a746288ac447f599b21bc4d16fd
RedDot15 <minhtb03@gmail.com>
2025-03-15 15:38:48 +0700

    commit again for the last commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NgFor } from '@angular/common';
-import { BoardComponent } from "./components/board/board.component";
+import { BoardComponent } from "./components/board/board.component"
+import { HttpClientModule } from '@angular/common/http';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
