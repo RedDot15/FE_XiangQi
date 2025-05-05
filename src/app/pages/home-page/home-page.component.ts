f3652d5e955a6b14b24db7d05b4a554747fc678d
StarEater2003 <stareater2003@gmail.com>
2025-05-05 11:05:21 +0700

    home_page_UI

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { BoardComponent } from '../../components/board/board.component';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-home-page',
   standalone: true,
@@ -11,5 +11,16 @@ import { BoardComponent } from '../../components/board/board.component';
   styleUrl: './home-page.component.css'
 })
 export class HomePageComponent {
+  constructor(public router:Router){
+  }
+  onNavigate(path:any){
+    this.router.navigate([path])
+  }
+  onPlayOnline() {
+    this.router.navigate(['/play/PvP']);
+  }
 
+  onPlayWithAI() {
+    this.router.navigate(['/play/computer']);
+  }
 }
